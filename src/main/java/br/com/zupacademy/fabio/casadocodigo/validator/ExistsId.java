f8ef94a0b56486c8b87af287118801a56266d475
Fabio Santos de Souza <fabio.desouza@zup.com.br>
2021-08-25 11:22:00 -0300

    refatoracao e inclusao do existsIdValidator

---
+++ src/main/java/br/com/zupacademy/fabio/casadocodigo/validator/ExistsId.java
@@ -0,0 +1,2 @@
+package br.com.zupacademy.fabio.casadocodigo.validator;public interface ExistsId {
+}
---
+++ src/main/java/br/com/zupacademy/fabio/casadocodigo/validator/ExistsIdValidator.java
@@ -0,0 +1,2 @@
+package br.com.zupacademy.fabio.casadocodigo.validator;public class ExistsIdValidator {
+}
